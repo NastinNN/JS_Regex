e920e008ed90f7ac7d1aef16c21ab4c3d0bb32fa
NastinNN <nastya.mannova@gmail.com>
2023-11-09 16:58:33 +0300

    Решены задачи 3,4,5

--- JS/regex.js
+++ JS/regex.js
@@ -50,3 +50,58 @@ function task2() {
             return flag;
       }
 }
+
+function task3() {
+      let str=prompt("Введите строку");
+      if (isPredl(str))
+            console.log("Введенная строка состоит только из букв и цифр");
+      else
+            console.log("Введенная строка состоит не только из букв и цифр");
+
+      function isPredl(str)
+      {
+            let re= /^[A-Za-z0-9]+$/;
+            // Не использовала \w потому что он содержит также и нижнее подчеркивание
+            let flag=re.test(str);
+            return flag;
+      }
+}
+
+function task4() {
+      let str=prompt("Введите строку");
+      if (isPredl(str))
+            console.log("Введенная строка не содержит цифры и спецсимволы и содержит больше 10 символов");
+      else
+      console.log("Введенная строка содержит цифры и спецсимволы или длина строки меньше 10");
+
+      function isPredl(str)
+      {
+            let re= /^[A-za-z\s]{10,}$/;
+            // Строка может содержать только буквы латинского алфавита и пробелы, а также должна быть длиной от 10 символов
+            let flag=re.test(str);
+            return flag;
+      }
+}
+
+function task5() {
+      // Первый способ
+      let str=prompt("Введите строку");
+      console.log(`Введенная строка: ${str}`);
+      let regNum=/\d/g;
+      let checkNum=str.match(regNum);
+      let regLet=/[a-z]/gi;
+      let checkLet=str.match(regLet);
+      console.log(`Количество цифр в веденной строке: ${checkNum.length}`);
+      console.log(`Количество букв в веденной строке: ${checkLet.length}`);
+
+      // Второй способ (filter+test)
+      let strArr=str.split('');
+      let regNum2=/\d/;
+      let regLet2=/[a-z]/i;
+      let newArrNum=strArr.filter((elem) => (regNum2.test(elem)));
+      let newArrLet=strArr.filter((elem) => (regLet2.test(elem)));
+      console.log("_______");
+      console.log("Второй способ решения (filter+test)")
+      console.log(`Количество цифр в веденной строке ${newArrNum.length}`);
+      console.log(`Количество букв в веденной строке:${newArrLet.length}`);
+}
